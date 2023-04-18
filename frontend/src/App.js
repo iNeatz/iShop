79e6c84412ecc5649ea9336face29ae771900c8b
iNeatz <sahilshrestha77@gmail.com>
2023-04-18 16:31:12 +0545

    Auth & Profile Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,29 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} exact />
-            <Route path="/product/:id" element={<ProductScreen />} />
-            <Route path="/cart/:id?" element={<CartScreen />} />
-          </Routes>
-        </Container>
-      </main>
-      <Footer />
-    </Router>
-  );
+		<Router>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<Routes>
+						<Route path='/' element={<HomeScreen />} exact />
+						<Route path='/product/:id' element={<ProductScreen />} />
+						<Route path='/cart/:id?' element={<CartScreen />} />
+						<Route path='/login' element={<LoginScreen />} />
+						<Route path='/register' element={<RegisterScreen />} />
+						<Route path='/profile' element={<ProfileScreen />} />
+					</Routes>
+				</Container>
+			</main>
+			<Footer />
+		</Router>
+	)
 };
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,34 +1,55 @@
-import React from "react";
-import { LinkContainer } from "react-router-bootstrap";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
-  return (
-    <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-        <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>ProShop</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav className="ml-auto" style={{ marginLeft: "auto" }}>
-              <LinkContainer to="/cart">
-                <Nav.Link href="/cart">
-                  <i className="fas fa-shopping-cart"></i> Cart
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link href="/login">
-                  <i className="fas fa-user"></i> Sign in
-                </Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-  );
-};
+	const dispatch = useDispatch()
+	const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+  
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
 
-export default Header;
+	return (
+		<header>
+			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+				<Container>
+					<LinkContainer to='/'>
+						<Navbar.Brand>iShop</Navbar.Brand>
+					</LinkContainer>
+					<Navbar.Toggle aria-controls='navbarScroll' />
+					<Navbar.Collapse id='navbarScroll'>
+						<Nav className='ml-auto' style={{ marginLeft: 'auto' }}>
+							<LinkContainer to='/cart'>
+								<Nav.Link href='/cart'>
+									<i className='fas fa-shopping-cart'></i> Cart
+								</Nav.Link>
+							</LinkContainer>
+							{userInfo ? (
+								<NavDropdown title={userInfo.name} id='username'>
+									<LinkContainer to='/profile'>
+										<NavDropdown.Item>Profile</NavDropdown.Item>
+									</LinkContainer>
+									<NavDropdown.Item onClick={logoutHandler}>
+										Logout
+									</NavDropdown.Item>
+								</NavDropdown>
+							) : (
+								<LinkContainer to='/login'>
+									<Nav.Link href='/login'>
+										<i className='fas fa-user'></i> Sign in
+									</Nav.Link>
+								</LinkContainer>
+							)}
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	)
+}
+
+export default Header
